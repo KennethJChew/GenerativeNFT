047f68bb2e3368c4b0baec7a335e24aba585516b
kennethJChew <kennethjcjw@gmail.com>
2021-11-07 20:12:55 +0800

    added generation folder

---
+++ scripts/generation/library_installation.py
@@ -0,0 +1,117 @@
+
+import requests
+import os
+
+imagenet_1024 = True
+imagenet_16384 = True
+gumbel_8192 = True
+coco = True
+faceshq = True
+wikiart_1024 = True
+wikiart_16384 = True
+sflckr = True
+ade20k = True
+ffhq = True
+celebahq = True
+
+models = {"imagenet_1024":True,"imagenet_16384":False,"gumbel_8192":False,"coco":False,
+"faceshq":False,"wikiart_1024":False,"wikiart_16384":False,"sflckr":False,"ade20k":False,
+"ffhq":False,"celebahq":False}
+
+def is_downloadable(url):
+  h = requests.head(url=url,allow_redirects=True)
+  header = h.headers
+  print(header)
+  content_type = header.get("content-type")
+  if "text" in content_type.lower():
+    return False
+  if "html" in content_type.lower():
+    return False
+  print(url + " is downloadable")
+  return True
+
+def download(url,filename):
+  dl = requests.get(url=url,allow_redirects=True)
+  open(filename,"wb").write(dl.content)
+
+def install():
+  # if imagenet_1024 == True:
+  #   imagenet_1024_yaml_url = "https://heibox.uni-heidelberg.de/d/8088892a516d4e3baf92/files/?p=%2Fconfigs%2Fmodel.yaml&dl=1"
+  #   imagenet_1024_ckpt_url = "https://heibox.uni-heidelberg.de/d/8088892a516d4e3baf92/files/?p=%2Fckpts%2Flast.ckpt&dl=1"
+  #   if is_downloadable(imagenet_1024_yaml_url):
+  #     download(imagenet_1024_yaml_url,"vqgan_imagenet_f16_1024.yaml")
+    # if is_downloadable(imagenet_1024_ckpt_url):
+      # download(imagenet_1024_ckpt_url,"vqgan_imagenet_f16_1024.ckpt")
+  # if imagenet_16384:
+  #   imagenet_16384_yaml_url = "https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fconfigs%2Fmodel.yaml&dl=1"
+  #   imagenet_16384_ckpt_url = "https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fckpts%2Flast.ckpt&dl=1"
+  #   if is_downloadable(imagenet_16384_yaml_url):
+  #     download(imagenet_16384_yaml_url,"vqgan_imagenet_f16_16384.yaml")
+    # if is_downloadable(imagenet_16384_yaml_url):
+      # download(imagenet_16384_ckpt_url,"vqgan_imagenet_f16_16384.ckpt")
+  # if gumbel_8192:
+  #   gumbel_8192_yaml_url = "https://heibox.uni-heidelberg.de/d/2e5662443a6b4307b470/files/?p=%2Fconfigs%2Fmodel.yaml&dl=1"
+  #   gumbel_8192_ckpt_url = "https://heibox.uni-heidelberg.de/d/2e5662443a6b4307b470/files/?p=%2Fckpts%2Flast.ckpt&dl=1"
+  #   if is_downloadable(gumbel_8192_yaml_url):
+  #     download(gumbel_8192_yaml_url,"gumbel_8192.yaml")
+    # if is_downloadable(gumbel_8192_ckpt_url):
+    #   download(gumbel_8192_ckpt_url,"gumbel_8192.ckpt")
+  # if coco:
+  #   coco_yaml_url = "https://dl.nmkd.de/ai/clip/coco/coco.yaml"
+  #   coco_ckpt_url = "https://dl.nmkd.de/ai/clip/coco/coco.ckpt"
+  #   if is_downloadable(coco_yaml_url):
+  #     download(coco_yaml_url,"coco.yaml")
+    # if is_downloadable(coco_ckpt_url):
+    #   download(coco_ckpt_url,"coco.ckpt")
+  if faceshq:
+    faceshq_yaml_url = "https://drive.google.com/uc?export=download&id=1fHwGx_hnBtC8nsq7hesJvs-Klv-P0gzT"
+    faces_ckpt_url = "https://app.koofr.net/content/links/a04deec9-0c59-4673-8b37-3d696fe63a5d/files/get/last.ckpt?path=%2F2020-11-13T21-41-45_faceshq_transformer%2Fcheckpoints%2Flast.ckpt"
+    if is_downloadable(faceshq_yaml_url):
+      download(faceshq_yaml_url,"faceshq.yaml")
+    # if is_downloadable(faces_ckpt_url):
+    #   download(faces_ckpt_url,"faceshq.ckpt")
+  # if wikiart_1024:
+  #   wikiart_1024_yaml_url = "http://mirror.io.community/blob/vqgan/wikiart.yaml"
+  #   wikiart_1024_ckpt_url = "http://mirror.io.community/blob/vqgan/wikiart.ckpt"
+  #   if is_downloadable(wikiart_1024_yaml_url):
+  #     download(wikiart_1024_yaml_url,"wikiart_1024.yaml")
+    # if is_downloadable(wikiart_1024_ckpt_url):
+    #   download(wikiart_1024_ckpt_url,"wikiart_1024.ckpt")
+  if wikiart_16384:
+    wikiart_16384_yaml_url = "http://eaidata.bmk.sh/data/Wikiart_16384/wikiart_f16_16384_8145600.yaml"
+    wikiart_16384_ckpt_url = "http://eaidata.bmk.sh/data/Wikiart_16384/wikiart_f16_16384_8145600.ckpt"
+    if is_downloadable(wikiart_16384_yaml_url):
+      download(wikiart_16384_yaml_url,"wikiart_16384.yaml")
+    # if is_downloadable(wikiart_16384_ckpt_url):
+    #   download(wikiart_16384_ckpt_url,"wikiart_16384.ckpt")
+  if sflckr:
+    sflckr_yaml_url = "https://heibox.uni-heidelberg.de/d/73487ab6e5314cb5adba/files/?p=%2Fconfigs%2F2020-11-09T13-31-51-project.yaml&dl=1"
+    sflckr_ckpt_url = "https://heibox.uni-heidelberg.de/d/73487ab6e5314cb5adba/files/?p=%2Fcheckpoints%2Flast.ckpt&dl=1"
+    if is_downloadable(sflckr_yaml_url):
+      download(sflckr_yaml_url,"sflckr.yaml")
+    # if is_downloadable(sflckr_ckpt_url):
+    #   download(sflckr_ckpt_url,"sflckr.ckpt")
+  if ade20k:
+    ade20k_yaml_url = "https://static.miraheze.org/intercriaturaswiki/b/bf/Ade20k.txt"
+    ade20k_ckpt_url = "https://app.koofr.net/content/links/0f65c2cd-7102-4550-a2bd-07fd383aac9e/files/get/last.ckpt?path=%2F2020-11-20T21-45-44_ade20k_transformer%2Fcheckpoints%2Flast.ckpt"
+    if is_downloadable(ade20k_yaml_url):
+      download(ade20k_yaml_url,"ade20k.yaml")
+    # if is_downloadable(ade20k_ckpt_url):
+    #   download(ade20k_ckpt_url,"ade20k.ckpt")
+  if ffhq:
+    ffhq_yaml_url = "https://app.koofr.net/content/links/0fc005bf-3dca-4079-9d40-cdf38d42cd7a/files/get/2021-04-23T18-19-01-project.yaml?path=%2F2021-04-23T18-19-01_ffhq_transformer%2Fconfigs%2F2021-04-23T18-19-01-project.yaml&force"
+    ffhq_ckpt_url = "https://app.koofr.net/content/links/0fc005bf-3dca-4079-9d40-cdf38d42cd7a/files/get/last.ckpt?path=%2F2021-04-23T18-19-01_ffhq_transformer%2Fcheckpoints%2Flast.ckpt&force"
+    if is_downloadable(ffhq_yaml_url):
+      download(ffhq_yaml_url,"ffhq.yaml")
+    # if is_downloadable(ffhq_ckpt_url):
+      # download(ffhq_ckpt_url,"ffhq.ckpt")
+  if celebahq:
+    celebahq_yaml_url = "https://app.koofr.net/content/links/6dddf083-40c8-470a-9360-a9dab2a94e96/files/get/2021-04-23T18-11-19-project.yaml?path=%2F2021-04-23T18-11-19_celebahq_transformer%2Fconfigs%2F2021-04-23T18-11-19-project.yaml&force"
+    celebahq_ckpt_url = "https://app.koofr.net/content/links/6dddf083-40c8-470a-9360-a9dab2a94e96/files/get/last.ckpt?path=%2F2021-04-23T18-11-19_celebahq_transformer%2Fcheckpoints%2Flast.ckpt&force"
+    if is_downloadable(celebahq_yaml_url):
+      download(celebahq_yaml_url,"celebahq.yaml")
+    # if is_downloadable(celebahq_ckpt_url):
+      # download(celebahq_ckpt_url,"celebahq.ckpt")
+
+def main():
+  install()
